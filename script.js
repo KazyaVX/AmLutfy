501222653c86c112627aa03399664129f050d1d9
KazyaVX <kazuyaicikawa@gmail.com>
2024-11-21 11:25:45 +0700

    Make the new section, and some fixed

--- script.js
+++ script.js
@@ -27,3 +27,27 @@ const themeToggle = document.getElementById("theme-toggle");
 themeToggle.addEventListener("click", () => {
   document.body.classList.toggle("dark");
 });
+
+// Fungsi untuk mendapatkan waktu sekarang
+    function getCurrentTime() {
+      var currentDate = new Date();
+      var hours = currentDate.getHours();
+      return hours;
+    }
+
+    // Fungsi untuk menampilkan salam berdasarkan waktu
+    function displayGreeting() {
+      var currentHour = getCurrentTime();
+      var greetingElement = document.getElementById("greeting");
+
+      if (currentHour >= 5 && currentHour < 12) {
+        greetingElement.textContent = "Selamat Pagi!";
+      } else if (currentHour >= 12 && currentHour < 18) {
+        greetingElement.textContent = "Selamat Siang!";
+      } else {
+        greetingElement.textContent = "Selamat Malam!";
+      }
+    }
+
+    // Panggil fungsi untuk menampilkan salam
+    displayGreeting();
